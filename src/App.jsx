51ea30e5d3fd92fc0d1a26bd9e017af908dbf22c
Tiendao <clonedopdz@gmail.com>
2023-10-28 14:21:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+const App = () => {
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default App;
